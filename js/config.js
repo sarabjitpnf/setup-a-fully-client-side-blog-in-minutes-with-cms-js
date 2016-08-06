ac3b8d180fa575566f7925b0cc94b5e1ef2b5d4f
kezzbracey <kbracey@gmail.com>
2016-08-06 15:16:16 +1000

    Modified config.js settings
    Initial configuration settings.

--- js/config.js
+++ js/config.js
@@ -3,20 +3,20 @@ $(function() {
   CMS.init({
 
     // Name of your site or location of logo file, relative to root directory (img/logo.png)
-    siteName: 'My Site',
+    siteName: '5 Minute Blog',
 
     // Tagline for your site
-    siteTagline: 'Your site tagline',
+    siteTagline: 'Using CMS.js',
 
     // Email address
-    siteEmail: 'your_email@example.com',
+    siteEmail: 'kezz@notmyrealemail.com',
 
     // Name
-    siteAuthor: 'Your Name',
+    siteAuthor: 'Kezz Bracey',
 
     // Navigation items
     siteNavItems: [
-      { name: 'Github', href: 'https://github.com/yourname', newWindow: false},
+      { name: 'My Tuts and Courses', href: 'http://tutsplus.com/authors/kezz-bracey', newWindow: true},
       { name: 'About'}
     ],
 
@@ -53,8 +53,8 @@ $(function() {
 
      // If Github mode is set, your Github username and repo name.
     githubUserSettings: {
-      username: 'yourusername',
-      repo: 'yourrepo'
+      username: 'kezzbracey',
+      repo: '5_minute_blog'
     },
 
     // If Github mode is set, choose which Github branch to get files from.
